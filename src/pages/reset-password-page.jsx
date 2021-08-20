eac6670e2be9608f5314408fae5c780279949055
Elena Chestikova <Chestikova.elena@gmail.com>
2021-08-20 18:33:48 +0500

    made ResetPasswordPage

--- src/pages/reset-password-page.jsx
+++ src/pages/reset-password-page.jsx
@@ -1,7 +1,63 @@
+import { useState } from 'react';
+import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import FormWrapper from '../components/form-wrapper';
+import SpanWithLink from '../components/span-with-link';
+
 export const ResetPasswordPage = () => {
+  const [state, setState] = useState({
+    newPassword: '',
+    code: ''
+  });
+
+  const handleInputChange = (e) => {
+    const target = e.target;
+    setState({
+      ...state,
+      [target.name]: target.value
+    });
+  }
+
   return (
-    <div className="text text_type_digits-large mt-15">
-      Сбросить пароль
-    </div>
+    <FormWrapper title="Восстановление пароля">
+      <div className="mb-6">
+        <Input
+          type={'password'}
+          placeholder={'Введите новый пароль'}
+          onChange={handleInputChange}
+          icon={'ShowIcon'}
+          value={state.newPassword}
+          name={'newPassword'}
+          error={false}
+          onIconClick={onIconClick}
+          errorText={''}
+          size={'default'}
+        />
+      </div>
+      <div className="mb-6">
+        <Input
+          type={'text'}
+          placeholder={'Введите код из письма'}
+          onChange={handleInputChange}
+          icon={null}
+          value={state.code}
+          name={'code'}
+          error={false}
+          onIconClick={onIconClick}
+          errorText={''}
+          size={'default'}
+        />
+      </div>
+      <div className="mb-20">
+        <Button type="primary" size="medium">
+          Сохранить
+        </Button>
+      </div>
+      <SpanWithLink
+        buttonText="Войти"
+        link="/login"
+        spanText="Вспомнили пароль?"
+        mb="0"
+      />
+    </FormWrapper>
   )
 }
