f687230586540a0f2970368a4dd169d8920bdafe
Elena Chestikova <Chestikova.elena@gmail.com>
2021-08-20 18:25:42 +0500

    made the ForgotPasswordPage

--- src/pages/forgot-password-page.jsx
+++ src/pages/forgot-password-page.jsx
@@ -1,7 +1,47 @@
+import { useState } from 'react';
+import { Button, Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import FormWrapper from '../components/form-wrapper';
+import SpanWithLink from '../components/span-with-link';
+
 export const ForgotPasswordPage = () => {
+  const [state, setState] = useState({
+    email: ''
+  });
+
+  const handleInputChange = (e) => {
+    const target = e.target;
+    setState({
+      ...state,
+      [target.name]: target.value
+    });
+  }
+
   return (
-    <div className="text text_type_digits-large mt-15">
-      Забыли пароль
-    </div>
+    <FormWrapper title="Восстановление пароля">
+      <div className="mb-6">
+        <Input
+          type={'email'}
+          placeholder={'Укажите e-mail'}
+          onChange={handleInputChange}
+          icon={null}
+          value={state.email}
+          name={'email'}
+          error={false}
+          errorText={''}
+          size={'default'}
+        />
+      </div>
+      <div className="mb-20">
+        <Button type="primary" size="medium">
+          Восстановить
+        </Button>
+      </div>
+      <SpanWithLink
+        buttonText="Войти"
+        link="/login"
+        spanText="Вспомнили пароль?"
+        mb="0"
+      />
+    </FormWrapper>
   )
 }
